4394833e23ad80df4706c4a634d0bc4fb3609ab5
Elisabeth 'TerraNova' Schulz <schulz.elisabeth.terra@gmail.com>
2026-06-07 08:44:19 +0200

    Rewrite to use openat2, many nifty features in the newer syscall

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ use dbus::message::MatchRule;
 use dbus::{Message, Path};
 use nix::dir::Dir;
 use nix::errno::Errno;
-use nix::fcntl::{OFlag, open};
-use nix::sys::stat::{Mode, SFlag, fstat};
+use nix::fcntl::{OFlag, OpenHow, ResolveFlag, open, openat2};
+use nix::sys::stat::{FileStat, Mode, SFlag, fstat};
 use nix::unistd::{Gid, Group, Uid, User, fchown, getgrouplist};
-use std::collections::VecDeque;
 use std::ffi::CString;
-use std::os::unix::fs::MetadataExt;
+use std::os::fd::OwnedFd;
 use std::path::PathBuf;
+use std::rc::Rc;
 use std::time::Duration;
 
 fn detect_switch_to_session(msg: &Message) -> Option<Path<'_>> {
@@ -32,7 +32,6 @@ fn examine_session_for_user(connection: &Connection, session_path: Path) -> Opti
         "Get",
         ("org.freedesktop.login1.Session", "User"),
     );
-    call_message.set_no_reply(false);
     let response = connection
         .send_with_reply_and_block(call_message, Duration::from_millis(100))
         .ok()?;
@@ -45,7 +44,6 @@ fn examine_session_for_user(connection: &Connection, session_path: Path) -> Opti
         "Get",
         ("org.freedesktop.login1.User", "UID"),
     );
-    call_message.set_no_reply(false);
     let response = connection
         .send_with_reply_and_block(call_message, Duration::from_millis(100))
         .ok()?;
@@ -61,103 +59,115 @@ fn potentially_change_ownership(conn: &Connection, msg: &Message) -> Option<u64>
     let uid_to_switch = examine_session_for_user(conn, session)?;
     println!("Session owned by {uid_to_switch}, checking users group");
     let uid = Uid::from_raw(uid_to_switch);
-    let gid = session_owner_is_in_users_group(uid)?;
+    let gid = get_group_list_for_user(uid)
+        .ok()?
+        .into_iter()
+        .find(|g| g.name == "users")?
+        .gid;
     println!("Switching to {uid}:{gid}");
-    let count = perform_chown(uid, gid);
-    Some(count)
+    perform_chown(uid, gid)
+        .inspect_err(|err| {
+            eprintln!(
+                "Failed to initiate filesystem walk, /opt/steamlib absent or symlink?: {err}"
+            );
+        })
+        .ok()
 }
 
 fn handle_seat_change<'a>(_signal: (), conn: &Connection, msg: &Message) -> bool {
-    match potentially_change_ownership(conn, msg) {
-        Some(count) => {
-            println!("Updated ownership to {count} files");
-        }
-        _ => {
-            // nothing to do
-        }
-    }
+    potentially_change_ownership(conn, msg)
+        .inspect(|count| println!("Successfully changed {count} file owners"));
 
     true
 }
 
-fn perform_chown(uid: Uid, gid: Gid) -> u64 {
+#[derive(Debug)]
+enum TraversalElement {
+    Root(OwnedFd, FileStat),
+    PendingChildNode(Rc<Dir>, CString),
+}
+
+impl TraversalElement {
+    fn resolve(self) -> nix::Result<(OwnedFd, FileStat)> {
+        match self {
+            TraversalElement::Root(fd, stat) => Ok((fd, stat)),
+            TraversalElement::PendingChildNode(parent, name) => {
+                let fd = openat2(
+                    &parent.as_ref(),
+                    name.as_c_str(),
+                    OpenHow::new().flags(OFlag::O_RDONLY).resolve(
+                        ResolveFlag::RESOLVE_NO_SYMLINKS
+                            | ResolveFlag::RESOLVE_NO_XDEV
+                            | ResolveFlag::RESOLVE_BENEATH
+                            | ResolveFlag::RESOLVE_NO_MAGICLINKS,
+                    ),
+                )?;
+                let stat = fstat(&fd)?;
+                Ok((fd, stat))
+            }
+        }
+    }
+}
+
+fn perform_chown(uid: Uid, gid: Gid) -> Result<u64, Errno> {
     let mut count = 0u64;
     let start_path = PathBuf::from("/opt/steamlib/");
-    let expected_device = start_path
-        .metadata()
-        .expect("/opt/steamlib must exist")
-        .dev();
 
-    let mut stack = vec![start_path];
+    let fd = open(
+        &start_path,
+        OFlag::O_RDONLY | OFlag::O_NOFOLLOW,
+        Mode::empty(),
+    )?;
+    let stat = fstat(&fd)?;
+    let mut stack = vec![TraversalElement::Root(fd, stat)];
 
-    while let Some(path) = stack.pop() {
-        match process_next_file(&path, &mut stack, expected_device, uid, gid) {
-            Ok(n) => {
-                count += n;
+    while let Some(element) = stack.pop() {
+        match process_next_file(element, &mut stack, &mut count, uid, gid) {
+            Ok(_) | Err(Errno::ELOOP) | Err(Errno::ENOENT) | Err(Errno::EXDEV) => {
+                // Success(ish) cases - we changed the file, it's gone, or we can't
             }
-            Err(Errno::EMLINK) | Err(Errno::ELOOP) => {
-                // nothing required, just a symlink we ignore
-            }
-            Err(errno) => {
-                eprintln!(
-                    "Could not change ownership of {} to {errno}",
-                    path.display()
-                );
+            Err(e) => {
+                eprintln!("Error in tree walk: {e}");
             }
         }
     }
 
-    count
+    Ok(count)
 }
 
 fn process_next_file(
-    path: &PathBuf,
-    queue: &mut Vec<PathBuf>,
-    expected_device: u64,
+    next: TraversalElement,
+    queue: &mut Vec<TraversalElement>,
+    count: &mut u64,
     user: Uid,
     group: Gid,
-) -> nix::Result<u64> {
-    let mut n = 0;
-    let fd = open(path, OFlag::O_RDONLY | OFlag::O_NOFOLLOW, Mode::empty())?;
-    // nofollow already removes all symlinks, no explicit check required
-    let file_stat = fstat(&fd)?;
-    if file_stat.st_dev != expected_device {
-        eprintln!(
-            "File {} device {} not equal to root device ({}). Suspicious, will not proceed",
-            path.display(),
-            file_stat.st_dev,
-            expected_device
-        );
-    } else {
-        let flags = SFlag::from_bits_truncate(file_stat.st_mode);
+) -> nix::Result<()> {
+    let (fd, file_stat) = next.resolve()?;
 
-        if file_stat.st_uid != user.as_raw() || file_stat.st_gid != group.as_raw() {
-            fchown(&fd, Some(user), Some(group))?;
-            n = 1;
-        }
+    if file_stat.st_uid != user.as_raw() || file_stat.st_gid != group.as_raw() {
+        fchown(&fd, Some(user), Some(group))?;
+        *count += 1;
+    }
 
-        if flags.contains(SFlag::S_IFDIR) {
-            let dir = Dir::from_fd(fd)?;
+    if SFlag::from_bits_truncate(file_stat.st_mode).contains(SFlag::S_IFDIR) {
+        let mut dir = Dir::from_fd(fd)?;
+        let entries = dir.iter().collect::<Vec<_>>();
+        let dir = Rc::new(dir);
 
-            for entry in dir {
-                let entry = entry?;
-                let mut next_path = path.clone();
-                let name = entry.file_name();
-
-                if c"." == name || c".." == name {
-                    continue;
-                }
-
-                let Ok(name) = name.to_str() else {
-                    eprintln!("UTF-8 fuckery on {name:?}, will not proceed");
-                    continue;
-                };
-                next_path.push(name);
-                queue.push(next_path);
+        for entry in entries {
+            let entry = entry?; // if directory walk fails for one entry, we just skip the rest
+            if c"." == entry.file_name() || c".." == entry.file_name() {
+                continue;
             }
+
+            queue.push(TraversalElement::PendingChildNode(
+                dir.clone(),
+                entry.file_name().to_owned(),
+            ));
         }
     }
-    Ok(n)
+
+    Ok(())
 }
 
 fn get_group_list_for_user(uid: Uid) -> nix::Result<Vec<Group>> {
@@ -178,13 +188,6 @@ fn get_group_list_for_user(uid: Uid) -> nix::Result<Vec<Group>> {
     }
 
     Ok(collected)
-}
-fn session_owner_is_in_users_group(uid: Uid) -> Option<Gid> {
-    if let Ok(list) = get_group_list_for_user(uid) {
-        list.iter().find(|g| g.name == "users").map(|g| g.gid)
-    } else {
-        None
-    }
 }
 
 fn main() {
